8c208ce319389a48e440bdfe6c5c7d5a3295f35f
Kuldeep-kd <Kuldeep-kd@live.com>
2018-05-29 00:21:22 +0530

    Added some new projects.

---
+++ Base64 Encoding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Base64_Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           
+
+            Console.WriteLine(Convert.ToInt32(Console.ReadLine(), int.Parse(Console.ReadLine())));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MoleculeToAtom/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoleculeToAtom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = "K4[ON(SO3)2]2";
+            Console.Write(f.Split('(', ')')[1]);
+            Console.ReadKey();
+        }
+
+
+        public static Dictionary<string, int> ParseMolecule(string formula)
+        {
+            var ans = new Dictionary<string, int>();
+            var molecules = new List<char>();
+
+            //                                                               K4[ON(SO3)2]2
+            while (formula == "")
+            {
+                string x = formula.Split('(', ')')[1];
+                if(char.IsLetter(x.First()) && char.IsDigit(x.ElementAt(1)))
+                {
+                    ans.Add(x.First().ToString(), x.ElementAt(1));
+                    
+                }
+
+
+
+
+            }
+
+
+
+            /*
+            foreach (char c in formula)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (!ans.ContainsKey(c.ToString()))
+                    { ans.Add(c.ToString(), 1); }
+
+                    
+
+                    
+                }
+                endloop:;
+            }
+            
+
+            */
+            
+
+            return ans;
+        }
+
+
+
+    }
+}
