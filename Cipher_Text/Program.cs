99091ce9a5a173489e5764eb932b313fdc13fd80
Kuldeep-kd <Kuldeep-kd@live.com>
2018-06-05 14:47:33 +0530

    New Cipher Text Project Added.

---
+++ Cipher_Text/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cipher_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cipher_text1("ABCDEFGHIJKLMN"));
+            Console.WriteLine(Cipher_text2("I LIKE YOU","PANDA"));
+            Console.ReadKey();
+        }
+
+        public static string Cipher_text2(string v1, string v2)
+        {
+            int keyflag=0,key,r;
+            string ans = "";
+            char[] keyarr = v2.ToArray();
+            foreach(char c in v1)
+            {
+                if (c == ' ')
+                {
+                    ans += " ";
+                }
+                else if (keyflag < keyarr.Length)
+                {
+                    
+                    key = ((int)keyarr[keyflag]) % 26;
+
+                    r = ((int)c + key) % 26;
+
+                    ans += (char)(r + 65);
+                    keyflag++;
+                }
+                else
+                    keyflag = 0;
+
+                
+            }
+
+
+            return ans;
+        }
+
+        public static string Cipher_text1(string hello)
+        {
+            string cipher="";
+            int x=0;
+
+            foreach( char c in hello)
+            {
+                x = (Convert.ToInt32(c) + 2 ) % 26;
+                //Console.WriteLine((char)(x+65));
+                cipher += ((char)(x + 65));
+            }
+
+            return cipher;
+        }
+
+    }
+}
--- MoleculeToAtom/Program.cs
+++ MoleculeToAtom/Program.cs
@@ -11,33 +11,63 @@ namespace MoleculeToAtom
         static void Main(string[] args)
         {
             var f = "K4[ON(SO3)2]2";
-            Console.Write(f.Split('(', ')')[1]);
+            //Console.Write(f.Split('(', ')')[1]);
+            var d = ParseMolecule(f);
+
+            foreach(KeyValuePair<string,int> c in d)
+            {
+                //Console.WriteLine("Key = {0} , Value = {1}", c.Key, c.Value);
+                Console.WriteLine("\n");
+            }
             Console.ReadKey();
         }
-
 
         public static Dictionary<string, int> ParseMolecule(string formula)
         {
             var ans = new Dictionary<string, int>();
             var molecules = new List<char>();
-
-            //                                                               K4[ON(SO3)2]2
-            while (formula == "")
+            int temp = 0;
+            
+            
+            while (formula != "")
             {
-                string x = formula.Split('(', ')')[1];
-                if(char.IsLetter(x.First()) && char.IsDigit(x.ElementAt(1)))
-                {
-                    ans.Add(x.First().ToString(), x.ElementAt(1));
-                    
+                string x = formula;//.Split('(', ')')[1];
+                
+                //      1.
+                if (char.IsLetter(x.First()) && char.IsDigit(x.ElementAt(1)))
+                        {
+                            if (ans.ContainsKey(x.First().ToString()))
+                            {
+                                temp = x.ElementAt(1) + ans[x.First().ToString()];
+                                ans.Add(x.First().ToString(), temp);
+                                x = x.Replace(x.First().ToString(), "").Replace(x.First().ToString(),"");
+                            }
+                            else
+                            {
+                                ans.Add(x.First().ToString(), int.Parse(x.ElementAt(1).ToString()));
+                                x = x.Replace(x.First().ToString(), "");
+                                x = x.Replace(x.First().ToString(), "");
+                    }
+                        }
+                //      2.
+                else if (char.IsLetter(x.First()) && !char.IsDigit(x.ElementAt(1)))
+                        {
+
+                    if (ans.ContainsKey(x.First().ToString()))
+                    {
+                        temp = 1 + ans[x.First().ToString()];
+                        ans.Remove(x.First().ToString());
+                        ans.Add(x.First().ToString(), temp);
+                        x = x.Replace(x.First().ToString(), "").Replace(x.First().ToString(), "");
+                    }
+                    else
+                    {
+                        ans.Add(x.First().ToString(), int.Parse(x.ElementAt(1).ToString()));
+                        x = x.Replace(x.First().ToString(), "").Replace(x.First().ToString(), "");
+                    }
                 }
-
-
-
-
             }
-
-
-
+            
             /*
             foreach (char c in formula)
             {
@@ -45,22 +75,13 @@ namespace MoleculeToAtom
                 {
                     if (!ans.ContainsKey(c.ToString()))
                     { ans.Add(c.ToString(), 1); }
-
-                    
-
-                    
+        
                 }
                 endloop:;
             }
             
-
             */
-            
-
             return ans;
         }
-
-
-
     }
 }
